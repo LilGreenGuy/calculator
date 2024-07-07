063dc63ca0377f58116c3ae7ea8b8c05bdbbc5c6
Lil Green Guy <139634331+LilGreenGuy@users.noreply.github.com>
2024-07-07 16:34:12 -0500

    Pushing an early build to github so I can easily grab it on my personal computer.

--- app.js
+++ app.js
@@ -1,70 +1,125 @@
 const calcDisplay = document.querySelector('.calcDisplay');
-let displayNums = '0'
-let secondNum = ''
+// let displayNum = '0'
+let firstNums = ''
+let secondNums = '';
+let numSwitch = 0;
+let mode = 'none'
 
-const inputs = {
-leftParenBtn: document.querySelector('#leftParen'),
-rightParenBtn: document.querySelector('#rightParen'),
-percentBtn: document.querySelector('#percent'),
-clearBtn: document.querySelector('#clear'),
-sevenBtn: document.querySelector('#seven'),
-eightBtn: document.querySelector('#eight'),
-nineBtn: document.querySelector('#nine'),
-divideBtn: document.querySelector('#divide'),
-fourBtn: document.querySelector('#four'),
-fiveBtn: document.querySelector('#five'),
-sixBtn: document.querySelector('#six'),
-multiplyBtn: document.querySelector('#multiply'),
-oneBtn: document.querySelector('#one'),
-twoBtn: document.querySelector('#two'),
-threeBtn: document.querySelector('#three'),
-minusBtn: document.querySelector('#minus'),
-zeroBtn: document.querySelector('#zero'),
-decimalBtn: document.querySelector('#decimal'),
-equalBtn: document.querySelector('#equals'),
-plusBtn: document.querySelector('#plus')
-}
-
-window.addEventListener('keyup', (e) => {
-    try{
-        console.log(e.key)
-        if (Number.isInteger(parseInt(e.key)) && displayNums.length < 12) {
-            if (displayNums[0] === '0') {
-                displayNums = displayNums.substring(displayNums.length)
-            }
-            displayNums += e.key
-            updateDisplay(displayNums)
-        }
-        if (e.key === 'Backspace') {
-            if (displayNums.length > 1) {
-                displayNums = displayNums.slice(0, -1)
-                updateDisplay()
-            } else if (displayNums.length === 1){
-                displayNums = '0';
-                updateDisplay();
-            }
-        }
-    } catch (e) {
-        console.log(e)
-    }
+const numberInputs = Array.from(document.querySelectorAll('[data-number]'))
+numberInputs.forEach(input => {
+    input.addEventListener('click', () => {
+        addToDisplay(input.dataset.number)
+    })
 })
 
-inputs.clearBtn.addEventListener('click', () => clearDisplay())
-
-function clearDisplay() {
-    displayNums = '0'
-    updateDisplay()
+const inputs = {
+    leftParenBtn: document.querySelector('#leftParen'),
+    rightParenBtn: document.querySelector('#rightParen'),
+    percentBtn: document.querySelector('#percent'),
+    clearBtn: document.querySelector('#clear'),
+    divideBtn: document.querySelector('#divide'),
+    multiplyBtn: document.querySelector('#multiply'),
+    minusBtn: document.querySelector('#minus'),
+    decimalBtn: document.querySelector('#decimal'),
+    equalBtn: document.querySelector('#equals'),
+    plusBtn: document.querySelector('#plus')
 }
 
-const updateDisplay = () => {
-    calcDisplay.innerHTML = displayNums
-    if (!displayNums[0]) {
-        calcDisplay.innerHTML = 0
+const eventListeners = [
+    window.addEventListener('keyup', (e) => {
+        console.log(e.key)
+        try {
+            if (!isNaN(e.key)) {
+                const key = e.key
+                addToDisplay(key)
+            }
+            if (e.key === 'Backspace') {
+                if (numSwitch === 0) {
+                    if (firstNums.length >= 1) {
+                        firstNums = firstNums.slice(0, -1)
+                        updateDisplay(firstNums)
+                        if (firstNums === '') {
+                            updateDisplay(0)
+                        }
+                    } else if (!firstNums.length) {
+                        updateDisplay(0)
+                    }
+                } else if (numSwitch === 1) {
+                    secondNums = secondNums.slice(0, -1)
+                    if(secondNums.length >= 1) {
+                        updateDisplay(secondNums)
+                    } else if (!secondNums.length) {
+                        updateDisplay(0);
+                    }
+                }
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    }),
+    inputs.clearBtn.addEventListener('click', () => clearDisplay()),
+    inputs.plusBtn.addEventListener('click', () => {
+        if (numSwitch !== 1) {
+            numSwitch = 1
+            mode = 'add'
+        }
+    }),
+    inputs.minusBtn.addEventListener('click', () => {
+        if (numSwitch !== 1) {
+            numSwitch = 1
+            mode = 'subtract'
+        }
+    }),
+    inputs.equalBtn.addEventListener('click', () => {
+        if (secondNums === '') {
+            secondNums = '0'
+        }
+        if (mode === 'add') {
+            add()
+            mode = 'none'
+            numSwitch = 0;
+            secondNums = ''
+        }
+        if (mode === 'subtract') {
+            subtract()
+            mode = 'none'
+            numSwitch = 0
+            secondNums = ''
+        }
+    })
+]
+
+function addToDisplay(numInput) {
+    if (numSwitch === 0) {
+        firstNums += numInput
+        updateDisplay(firstNums)
+        console.log(firstNums)
+    } else if (numSwitch === 1) {
+        secondNums += numInput
+        updateDisplay(secondNums)
     }
 }
 
-//math logicals
+function updateDisplay(value) {
+    console.log(value)
+    calcDisplay.innerHTML = value;
+}
 
-function add(num1, num2) {
+function clearDisplay() {
+    firstNums = '';
+    secondNums = '';
+    numSwitch = 0;
+    calcDisplay.innerHTML = '0';
+}
 
+function add() {
+    firstNums = parseInt(firstNums) + parseInt(secondNums);
+    secondNums = ''
+    updateDisplay(firstNums)
+}
+
+function subtract() {
+    firstNums = parseInt(firstNums) - parseInt(secondNums);
+    secondNums = ''
+    updateDisplay(firstNums)
 }
